867aa37abe39ed6c03e12231c8da1ea236e159e7
preetip26 <preeti.poddar@somaiya.edu>
2021-07-29 15:06:33 +0530

    Parameterized function added

--- Program.cs
+++ Program.cs
@@ -4,13 +4,19 @@ namespace ToBeTested
 {
     public class Program
     {
+        static string name;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the name: ");
+            name = Console.ReadLine();
         }
         public int Add(int x, int y)
         {
             return x + y;
+        }
+        public void SayHello(string who)
+        {
+            Console.WriteLine("Hello "+who);
         }
     }
     
